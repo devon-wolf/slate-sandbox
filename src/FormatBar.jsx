2f831d7c69fb0d2f1904dfe486b7dcc82562f0f9
Devon Wolfkiel <devonwolf@tutanota.com>
2021-05-30 22:25:59 -0700

    Refactor FormatBar component to use reusable function with callback

--- src/FormatBar.jsx
+++ src/FormatBar.jsx
@@ -4,51 +4,42 @@ import { CustomEditor } from './helpers';
 const { toggleCodeBlock, toggleBoldMark, toggleItal, toggleUnderline, toggleStrikethrough } = CustomEditor;
 
 const FormatBar = ({ editor }) => {
+
+	const handleToggle = (e, callback) => {
+		e.preventDefault();
+		callback(editor);
+	};
+
 	return (
 		<div aria-label="formatting tools" className="formatBar">
 
 			<button
 				aria-label="toggle bold"
-				onMouseDown={event => {
-					event.preventDefault();
-					toggleBoldMark(editor);
-			}}>
+				onMouseDown={e => handleToggle(e, toggleBoldMark)}>
 				<i title="bold" className="fas fa-bold"></i>
 			</button>
 
 			<button
 				aria-label="toggle italic"
-				onMouseDown={event => {
-					event.preventDefault();
-					toggleItal(editor);
-			}}>
+				onMouseDown={e => handleToggle(e, toggleItal)}>
 				<i title="italic" className="fas fa-italic"></i>
 			</button>
 
 			<button
 				aria-label="toggle underline"
-				onMouseDown={event => {
-					event.preventDefault();
-					toggleUnderline(editor);
-			}}>
+				onMouseDown={e => handleToggle(e, toggleUnderline)}>
 				<i title="underline" class="fas fa-underline"></i>
 			</button>
 
 			<button
 				aria-label="toggle strikethrough"
-				onMouseDown={event => {
-					event.preventDefault();
-					toggleStrikethrough(editor);
-			}}>
+				onMouseDown={e => handleToggle(e, toggleStrikethrough)}>
 				<i title="strikethrough" class="fas fa-strikethrough"></i>
 			</button>
 
 			<button
 				aria-label="toggle code block"
-				onMouseDown={event => {
-					event.preventDefault();
-					toggleCodeBlock(editor);
-			}}>
+				onMouseDown={e => handleToggle(e, toggleCodeBlock)}>
 				<i title="code" className="fas fa-code"></i>
 			</button>
 
